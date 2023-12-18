f12723cbcb722fe1a702303f59fe19e3f6bc3cc2
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-18 17:01:32 -0300

    Task 0 - api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,15 +12,18 @@ def get_employee_todo_progress(employee_id):
     user = requests.get(f'{base_url}/users/{employee_id}')
     todos = requests.get(f'{base_url}/todos?userId={employee_id}')
 
+    # Imprime la respuesta de la solicitud a la API de usuarios
+    print("User API response:", user.text)
+
     user_data = user.json()
     todos_data = todos.json()
 
-    employee_name = user_data['name']
+    employee_name = user_data.get('name')  # Usa get() para manejar claves ausentes
     all_employee = len(todos_data)
     tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    print(f"Employee {employee_name} is done with tasks\
-           ({tasks}/{all_employee}):")
+    print(f"Employee {employee_name} is done with tasks"
+          f" ({tasks}/{all_employee}):")
 
     for todo in todos_data:
         if todo['completed']:
@@ -28,5 +31,8 @@ def get_employee_todo_progress(employee_id):
 
 
 if __name__ == '__main__':
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+    else:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
