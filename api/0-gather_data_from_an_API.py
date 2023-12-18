8e86322615609fecf7e02e2d13b12714da0158bf
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-18 09:39:55 -0300

    Task 0 - Api - Fixes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,19 +9,19 @@ def get_employee_todo_progress(employee_id):
     """ get all employee """
     base_url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(f'{base_url}/users/{employee_id}')
-    users = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todos = requests.get(f'{base_url}/todos?userId={employee_id}')
 
     user_data = user.json()
-    users_data = user.json()
+    todos_data = todos.json()
 
     employee_name = user_data['name']
-    all_employee = len(users_data)
-    tasks = sum(1 for todo in users_data if todo['completed'])
+    all_employee = len(todos_data)
+    tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    print(f"Employee {employee_name} is done with tasks\
-        ({tasks}/{all_employee}):")
+    print(f"Employee {employee_name} is done with tasks "
+          f"({tasks}/{all_employee}):")
 
-    for todo in users_data:
+    for todo in todos_data:
         if todo['completed']:
             print(f"\t {todo['title']}")
 
