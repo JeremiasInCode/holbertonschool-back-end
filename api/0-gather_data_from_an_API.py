46f2442b8fa53bf30a2b884517ed28b803e4501b
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-20 15:06:52 -0300

    Task 0 changes urllib to requests

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
 """Returns information about his/her TODO list progress using urllib."""
 
-import json
-import sys
-from urllib.request import urlopen
+import requests
+from sys import argv
 
+if __name__ == '__main__':
+    """
+        Main Function of the program.
+    """
 
-def get_employee_todo_progress(employee_id):
-    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    base_url = "https://jsonplaceholder.typicode.com/todos?"
-    todo_url = f"{base_url}userId={employee_id}"
+    api_url = f'https://jsonplaceholder.typicode.com/'
 
-    with urlopen(api_url) as response:
-        user_data = json.load(response)
+    user_id = int(argv[1])
+    user_data = requests.get(api_url + f'users/{user_id}').json()
+    user_tasks = requests.get(api_url + f'users/{user_id}/todos').json()
 
-    with urlopen(todo_url) as response:
-        todo_data = json.load(response)
+    user_completed_tasks = [i for i in user_tasks if i['completed']]
 
-    employee_name = user_data.get("name")
-    completed_tasks = [task for task in todo_data if task.get("completed")]
-    total_tasks = len(todo_data)
+    print(f'Employee {user_data["name"]} is done with ', end='')
+    print(f'tasks({len(user_completed_tasks)}/{len(user_tasks)}):')
 
-    output = [
-        f"Employee {employee_name} is done with tasks("
-        f"{len(completed_tasks)}/{total_tasks}): "
-    ]
-
-    for task in completed_tasks:
-        output.append(f"\t {task['title']}")
-
-    return "\n".join(output)
-
-
-if len(sys.argv) != 2:
-    print("Usage: python3 script_name.py <employee_id>")
-else:
-    employee_id = int(sys.argv[1])
-    result = get_employee_todo_progress(employee_id)
-    print(result)
+    for task in user_completed_tasks:
+        print("\t " + task["title"])
